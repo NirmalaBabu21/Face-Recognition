7b133d187e01c26a739a0562ce9e0124704a16b3
Nirmala Babu <103834688+NirmalaBabu21@users.noreply.github.com>
2022-10-16 21:23:35 +0530

    Add files via upload

---
+++ model/server/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route('/classify_image', methods=['GET', 'POST'])
+def classify_image():
+    image_data = request.form['image_data']
+
+    response = jsonify(util.classify_image(image_data))
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Face Recognition Celebrity Image Classification")
+    util.load_saved_artifacts()
+    #from waitress import serve
+    #serve(app, host="0.0.0.0", port=8080)
+    app.run(port=5000)
